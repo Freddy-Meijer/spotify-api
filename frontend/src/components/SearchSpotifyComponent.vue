695e17aa5e198658e6cb7937fa640e3f975b5893
Freddy Meijer <freddymeijer@beewan.nl>
2022-12-11 13:30:53 +0100

    Adds SearchSpotify.vue component and emits result

---
+++ frontend/src/components/SearchSpotifyComponent.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+import {ref} from "vue"
+
+const emit = defineEmits<{
+  (e: "result", searchString: string): void;
+}>();
+
+const speechRecognitionSupported = ref<boolean>(true)
+const {webkitSpeechRecognition, SpeechRecognition} = (window as any)
+
+const recognition = ref()
+const result = ref<string>('')
+let listening = ref<boolean>(false);
+
+if ("SpeechRecognition" in window) {
+  recognition.value = new SpeechRecognition();
+} else if ("webkitSpeechRecognition" in window) {
+  recognition.value = new webkitSpeechRecognition();
+} else {
+  speechRecognitionSupported.value = false;
+}
+
+const startRecognition = () => {
+  try {
+    recognition.value.start();
+    listening.value = true;
+  } catch (e) {
+    console.log("Could not start recognition", e);
+  }
+}
+
+const stopRecognition = () => {
+  try {
+    recognition.value.stop();
+    listening.value = false
+  } catch (e) {
+    console.log("Could not stop recognition", e);
+  }
+}
+
+recognition.value.onresult = (event: any) => {
+  const transcript = event.results[0][0].transcript;
+  result.value = transcript;
+  listening.value = false;
+  emit("result", result.value);
+}
+
+recognition.value.onaudioend = () => {
+  listening.value = false
+}
+
+</script>
+
+<template>
+  <div id="speech-recognition">
+    <h3>Search</h3>
+    <div class="supported">
+      <div class="input-group mb-3">
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" v-model="result">
+          <span
+            class="input-group-text"
+            v-if="speechRecognitionSupported"
+            @click="listening ? stopRecognition() : startRecognition()">
+            {{ listening ? "Cancel" : "Voice Search" }}
+          </span>
+          <span class="input-group-text" v-else>Search</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/variables.scss";
+
+#speech-recognition {
+  h3 {
+    color: $spotify-green;
+  }
+
+  .input-group {
+    input {
+    }
+
+    .input-group-text {
+      cursor: pointer;
+    }
+  }
+}
+</style>
